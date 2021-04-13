e2e5127fabd6eb219d1ccdabe9eb0032df0d296f
Michel Atieno <mishqamish@gmail.com>
2021-04-13 13:23:28 +0300

    Able to view tasks onscreen

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './button'
 
 
 const header = ({ title}) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
     return (
-        <header>
-            <h1>{title}</h1>    
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
