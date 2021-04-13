69639074d5d4b824315e1dd5e56fb0fd099c68b5
Michel Atieno <mishqamish@gmail.com>
2021-04-13 12:11:00 +0300

    Added CSS

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+
+const header = ({ title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>    
+        </header>
+    )
+}
+
+header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+header.propTypes = {
+    title: PropTypes.string.isRequired,
+
+}
+
+export default header
