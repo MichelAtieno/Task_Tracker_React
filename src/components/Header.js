f876280798270a76e2d6cda88cf911052e3a1050
Michel Atieno <mishqamish@gmail.com>
2021-04-14 12:01:11 +0300

    Routing to about page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,30 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './button'
 
 
-const header = ({ title, onAdd, showAdd }) => {
-    
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color={showAdd ? 'red' : 'green'}
+            {location.pathname === '/' && (
+                <Button color={showAdd ? 'red' : 'green'}
                     text={showAdd ? 'Close' : 'Add'}
                     onClick={onAdd} />
+            )}
         </header>
     )
 }
 
-header.defaultProps = {
+Header.defaultProps = {
     title: 'Task Tracker',
 }
 
-header.propTypes = {
+Header.propTypes = {
     title: PropTypes.string.isRequired,
 
 }
 
-export default header;
+export default Header;
