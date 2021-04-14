8de585e4600cc466325ce241862fe9627268deda
Michel Atieno <mishqamish@gmail.com>
2021-04-14 07:51:20 +0300

    User able to save a task

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/header'
 import Tasks from './components/Tasks'
 import { useState } from 'react'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +25,11 @@ function App() {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -40,6 +46,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
                onDelete={deleteTask}
